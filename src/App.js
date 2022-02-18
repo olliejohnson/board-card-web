ed72e1a6dc94ef114002a96a7397a8b19c7e5397
olliejohnson <oliver.joseph.johnson@gmail.com>
2022-02-17 22:01:16 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Client } from 'boardgame.io/react';
+import { Local } from 'boardgame.io/multiplayer';
+import { TicTacToe } from './Game';
+import { TicTacToeBoard } from './Board';
+
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: Local(),
+});
+
+const App = () => (
+  <div>
+    <TicTacToeClient playerID='0' />
+    <TicTacToeClient playerID='1' />
+  </div>
+);
+
+export default App;
